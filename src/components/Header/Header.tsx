0718b6c1019111f7d1249a20f64d7d4334bafc4a
Minh Tran <thminhkg@gmail.com>
2022-12-30 16:11:51 +0700

    feat: layout and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,74 +7,84 @@ import { Text } from 'components/Text'
 import { useAuthContext } from 'context/auth'
 import React, { Fragment } from 'react'
 import { ProfileModal } from './ProfileModal'
+// import { Logo } from './Logo'
 
 export const Header = () => {
   const { logout, user } = useAuthContext()
   const { isOpen, onClose, onOpen } = useDisclosure()
 
   return (
-    <header className="py-2 px-5 flex justify-end mx-auto">
-      <Menu as="div" className="relative inline-block text-left">
-        <Menu.Button
-          className="inline-flex focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 items-center space-x-3"
-          data-testid="profile-button"
-        >
-          <img
-            src={user.avatar}
-            alt=""
-            width="40"
-            height="40"
-            className="rounded-full"
-          />
-          <Text as="span" className="text-gray-600 text-sm">
-            {user.firstName}
-          </Text>
-        </Menu.Button>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 w-48 mt-1 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="px-1 py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-pink-600 text-white' : 'text-gray-500'
-                    } group flex rounded-md items-center w-full px-2 py-2 text-sm space-x-2`}
-                    onClick={onOpen}
-                  >
-                    <IconPencilAlt className="w-5 h-5" />
-                    <span>Edit profile</span>
-                  </button>
-                )}
-              </Menu.Item>
-              <Divider className="h-0 mx-2 mb-px" />
-              <div />
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-pink-600 text-white' : 'text-gray-500'
-                    } group flex rounded-md items-center w-full px-2 py-2 text-sm space-x-2`}
-                    onClick={logout}
-                    data-testid="logout-button"
-                  >
-                    <IconLogout className="w-5 h-5" />
-                    <span>Logout</span>
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-      <ProfileModal isOpen={isOpen} onClose={onClose} />
+    <header className="py-2 px-5 border-b border-b-gray-800">
+      <div className="container mx-auto">
+        <div className="flex justify-between">
+          <span className="inline-flex items-center">
+            {/* <span className="w-8 h-8 inline-block mr-2">
+              <Logo />
+            </span> */}
+            <Text id="logo" as="strong">
+              Solana Grant
+            </Text>
+          </span>
+          <Menu as="div" className="relative inline-block text-left">
+            <Menu.Button
+              className="inline-flex focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 items-center space-x-3"
+              data-testid="profile-button"
+            >
+              <img
+                src={user.avatar}
+                alt=""
+                width="36"
+                height="36"
+                className="rounded-full"
+              />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 w-48 mt-1 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? 'bg-pink-600 text-white' : 'text-gray-500'
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm space-x-2`}
+                        onClick={onOpen}
+                      >
+                        <IconPencilAlt className="w-5 h-5" />
+                        <span>Edit profile</span>
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Divider className="h-0 mx-2 mb-px" />
+                  <div />
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? 'bg-pink-600 text-white' : 'text-gray-500'
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm space-x-2`}
+                        onClick={logout}
+                        data-testid="logout-button"
+                      >
+                        <IconLogout className="w-5 h-5" />
+                        <span>Logout</span>
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+          <ProfileModal isOpen={isOpen} onClose={onClose} />
+        </div>
+      </div>
     </header>
   )
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -7,7 +7,7 @@ export interface TextProps extends HTMLAttributes<HTMLElement> {
 }
 
 export const Text: React.FC<TextProps> = (props) => {
-  const { className = 'text-gray-900', truncate, as = 'p', ...rest } = props
+  const { className = 'text-white', truncate, as = 'p', ...rest } = props
   const classNames = []
   if (truncate) {
     classNames.push('truncate')
